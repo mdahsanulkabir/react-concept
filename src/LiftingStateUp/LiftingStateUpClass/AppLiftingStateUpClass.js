c1e2d6f7cd7e7674a034b9d4a23a87c5b6f9c82d
ahsan <makabir98@yahoo.com>
2022-11-18 08:15:24 +0600

    first commit

---
+++ src/LiftingStateUp/LiftingStateUpClass/AppLiftingStateUpClass.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './AppLiftingStateUpClass.module.css'
+
+class AppLiftingStateUpClass extends Component {
+    render() {
+        return (
+            <div className={styles.App}>
+                <h1>Hello</h1>
+            </div>
+        );
+    }
+}
+
+export default AppLiftingStateUpClass;
